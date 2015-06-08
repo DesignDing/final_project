1ca074146c3bed724c62787930b768a7f17b0f33
Gloryah Allen <gja2nd@virginia.edu>
2015-06-08 16:12:30 +0200

    Add controllers models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608094651) do
+ActiveRecord::Schema.define(version: 20150608140719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expenses", force: :cascade do |t|
+    t.date     "date"
+    t.string   "payer"
+    t.text     "description"
+    t.float    "amount"
+    t.integer  "trip_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "expenses", ["trip_id"], name: "index_expenses_on_trip_id", using: :btree
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "number"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +56,6 @@ ActiveRecord::Schema.define(version: 20150608094651) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "expenses", "trips"
+  add_foreign_key "trips", "users"
 end
